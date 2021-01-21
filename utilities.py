51556d84bcb24acc2de7befe9b26d894fc722b53
Joham Medina <joham.nba@gmail.com>
2021-01-21 13:07:53 -0500

    Adds value for messages of type: request and inbound

--- utilities.py
+++ utilities.py
@@ -5,7 +5,9 @@ class Message(enum.Enum):
     ERROR = "\033[1;31;40m" + "ERROR: "
     INFO = "\033[1;32;40m" + "INFO: "
     WARNING = "\033[1;33;40m" + "WARNING: "
-    RESET = "\033[0;37;49m \n"
+    REQUEST = "\033[1;36;40m" + "REQUEST: " + "\033[1;37;40m"
+    INPUT = "\033[1;34;40m"
+    RESET = "\033[0;37;49m"
 
 
 class BrowserDriver(enum.Enum):
